e598c12eb79cf51abfe4cbf678a025d9c0520bc1
suMin <sumin@pia.space>
2025-07-08 11:33:40 +0900

    fix: 폰트 적용

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,8 +16,6 @@
 <style>
 	:global(body) {
 		margin: 0;
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 		background-color: #f5f5f5;
 	}
 </style>
